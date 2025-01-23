86d666e90b42115fa91ad05c9aa97b9dbc3a983e
anys34 <anyeseong34@gmail.com>
2025-01-23 23:54:40 +0900

    init

---
+++ src/test/java/org/example/carrier/CarrierApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.carrier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrierApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
