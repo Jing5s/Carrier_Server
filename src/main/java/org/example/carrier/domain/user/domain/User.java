d870b80418cc58524aae79135623f289e3a8feb8
anys34 <anyeseong34@gmail.com>
2025-01-24 14:40:43 +0900

    feat:: user entity 생성

---
+++ src/main/java/org/example/carrier/domain/user/domain/User.java
@@ -0,0 +1,32 @@
+package org.example.carrier.domain.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_user")
+@Entity
+public class User {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 15, nullable = false)
+    private String nickname;
+
+    @Column(nullable = false)
+    private String picture;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String googleRefreshToken;
+}
---
+++ src/main/java/org/example/carrier/domain/user/domain/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package org.example.carrier.domain.user.domain.repository;
+
+import org.example.carrier.domain.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
