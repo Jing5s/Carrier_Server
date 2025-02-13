6b79e0540d38134fb8efe1f044dc42a52fcba317
anys34 <anyeseong34@gmail.com>
2025-02-13 15:44:45 +0900

    Fix:: color @NotNull로 변경

--- src/main/java/org/example/carrier/domain/category/presentation/dto/request/AddCategoryRequest.java
+++ src/main/java/org/example/carrier/domain/category/presentation/dto/request/AddCategoryRequest.java
@@ -1,6 +1,7 @@
 package org.example.carrier.domain.category.presentation.dto.request;
 
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import org.example.carrier.domain.category.domain.Category;
 import org.example.carrier.domain.category.domain.type.Color;
 import org.example.carrier.domain.user.domain.User;
@@ -9,7 +10,7 @@ public record AddCategoryRequest(
         @NotEmpty(message = "name이 비어있습니다.")
         String name,
 
-        @NotEmpty(message = "color가 비어있습니다.")
+        @NotNull(message = "color가 비어있습니다.")
         Color color
 ) {
     public Category toCategory(User user) {
