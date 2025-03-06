54a5d1e240ba0cea27ef8bb787c238cf8eed9125
anys34 <anyeseong34@gmail.com>
2025-03-07 03:34:52 +0900

    Feat:: mail schedule api

--- src/main/java/org/example/carrier/domain/mail/presentation/QueryMailController.java
+++ src/main/java/org/example/carrier/domain/mail/presentation/QueryMailController.java
@@ -1,10 +1,11 @@
 package org.example.carrier.domain.mail.presentation;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.example.carrier.domain.mail.presentation.dto.response.GetMailsResponse;
 import org.example.carrier.domain.mail.service.QueryMailService;
 import org.example.carrier.domain.user.facade.UserFacade;
-import org.example.carrier.global.feign.gmail.dto.response.GmailDetailResponse;
+import org.example.carrier.global.feign.gpt.dto.response.GptMailScheduleResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +22,12 @@ public class QueryMailController {
     @GetMapping
     public List<GetMailsResponse> getMails() {
         return queryMailService.getMails(UserFacade.getCurrentUser());
+    }
+
+    @GetMapping("/schedule/{gmail-id}")
+    public GptMailScheduleResponse getMailSchedule(
+            @PathVariable("gmail-id") String gmailId
+    ) throws JsonProcessingException {
+        return queryMailService.getMailSchedule(gmailId, UserFacade.getCurrentUser());
     }
 }
--- src/main/java/org/example/carrier/domain/mail/service/QueryMailService.java
+++ src/main/java/org/example/carrier/domain/mail/service/QueryMailService.java
@@ -1,21 +1,56 @@
 package org.example.carrier.domain.mail.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.example.carrier.domain.mail.domain.Mail;
 import org.example.carrier.domain.mail.domain.repository.CustomMailRepository;
+import org.example.carrier.domain.mail.domain.repository.MailRepository;
+import org.example.carrier.domain.mail.exception.MailNotFoundException;
 import org.example.carrier.domain.mail.presentation.dto.response.GetMailsResponse;
 import org.example.carrier.domain.user.domain.User;
+import org.example.carrier.domain.user.facade.GoogleOAuthFacade;
 import org.example.carrier.global.annotation.CustomService;
+import org.example.carrier.global.config.properties.GptProperties;
+import org.example.carrier.global.feign.gmail.GmailAPIClient;
+import org.example.carrier.global.feign.gmail.dto.response.GmailDetailResponse;
+import org.example.carrier.global.feign.gpt.GptClient;
+import org.example.carrier.global.feign.gpt.dto.request.GptBasicRequest;
+import org.example.carrier.global.feign.gpt.dto.request.GptMailScheduleRequest;
+import org.example.carrier.global.feign.gpt.dto.response.GptBasicResponse;
+import org.example.carrier.global.feign.gpt.dto.response.GptMailScheduleResponse;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @CustomService(readOnly = true)
 public class QueryMailService {
-    private final CustomMailRepository mailRepository;
+    private final MailRepository mailRepository;
+    private final CustomMailRepository customMailRepository;
+    private final GmailAPIClient gmailAPIClient;
+    private final GoogleOAuthFacade googleOAuthFacade;
+    private final ObjectMapper objectMapper;
+    private final GptClient gptClient;
+    private final GptProperties gptProperties;
 
     public List<GetMailsResponse> getMails(User cUser) {
-        return mailRepository.findAllByUserOrderByDate(cUser).stream()
+        return customMailRepository.findAllByUserOrderByDate(cUser).stream()
                 .map(GetMailsResponse::of)
                 .toList();
+    }
+
+    public GptMailScheduleResponse getMailSchedule(String gmailId, User cUser) throws JsonProcessingException {
+        String accessToken = googleOAuthFacade.getGoogleAccessToken(cUser);
+        Mail mail = mailRepository.findByGmailId(gmailId)
+                .orElseThrow(() -> MailNotFoundException.EXCEPTION);
+
+        GmailDetailResponse gmailDetail = gmailAPIClient.getGmailDetail(gmailId, accessToken);
+
+        GptBasicRequest request = GptMailScheduleRequest.of(
+                objectMapper, mail.getGmailId(), mail.getTitle(), gmailDetail.payload().getBody());
+
+        GptBasicResponse gptResponse = gptClient.getGptResponse("Bearer " + gptProperties.getToken(), request);
+
+        return (GptMailScheduleResponse) gptResponse.getResponse(objectMapper, GptMailScheduleResponse.class);
     }
 }
