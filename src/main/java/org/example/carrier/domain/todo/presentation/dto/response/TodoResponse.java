781397b5ee80dac14aa0a6d6f184b7b72cf0002e
anys34 <anyeseong34@gmail.com>
2025-03-11 11:04:28 +0900

    fix:: todo response 날짜 값 추가

--- src/main/java/org/example/carrier/domain/todo/presentation/dto/response/TodoResponse.java
+++ src/main/java/org/example/carrier/domain/todo/presentation/dto/response/TodoResponse.java
@@ -2,12 +2,15 @@ package org.example.carrier.domain.todo.presentation.dto.response;
 
 import org.example.carrier.domain.todo.domain.Todo;
 
+import java.time.LocalDate;
+
 public record TodoResponse(
         Long id,
         String title,
-        Boolean isDone
+        Boolean isDone,
+        LocalDate date
 ) {
     public TodoResponse(Todo todo) {
-        this(todo.getId(), todo.getTitle(), todo.getIsDone());
+        this(todo.getId(), todo.getTitle(), todo.getIsDone(), todo.getDate());
     }
 }
