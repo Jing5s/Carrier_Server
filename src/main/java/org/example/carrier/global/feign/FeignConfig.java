26af749b8383b57970eacde9edf536929bc3b0f3
anys34 <anyeseong34@gmail.com>
2025-03-17 10:38:34 +0900

    Feat:: timeout 늘리기

--- src/main/java/org/example/carrier/global/feign/FeignConfig.java
+++ src/main/java/org/example/carrier/global/feign/FeignConfig.java
@@ -1,5 +1,6 @@
 package org.example.carrier.global.feign;
 
+import feign.Request;
 import feign.codec.ErrorDecoder;
 import org.example.carrier.global.interceptor.ContentLengthRequestInterceptor;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -19,4 +20,10 @@ public class FeignConfig {
     public ContentLengthRequestInterceptor contentLengthRequestInterceptor() {
         return new ContentLengthRequestInterceptor();
     }
+
+    @Bean
+    public Request.Options feignOptions() {
+        return new Request.Options(30_000, 30_000); // 10초 연결 타임아웃, 30초 읽기 타임아웃
+    }
+
 }
