0ce4f5421b25d8d07281820a23046e76b0297a8a
anys34 <anyeseong34@gmail.com>
2025-04-15 09:37:17 +0900

    feat(cloud): domain 변경

--- src/main/java/org/example/carrier/global/utils/NextCloudService.java
+++ src/main/java/org/example/carrier/global/utils/NextCloudService.java
@@ -46,7 +46,7 @@ public class NextCloudService {
         return new UploadFileResponse(
                 fileName,
                 shareUrlResponse.getUrl()
-                        .replace(nextCloudProperties.getBaseUrl(), "https://nas.anys.kro.kr")
+                        .replace(nextCloudProperties.getBaseUrl(), "https://cloud.jamkris.kro.kr")
                         + "/download"
         );
     }
